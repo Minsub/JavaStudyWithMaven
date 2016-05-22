82a3f0a2ec674a0ae49f1d1dc25357d52eb02fad
jiminsub <jiminsub@gmail.com>
2016-05-22 21:59:41 +0900

    Initial commit

---
+++ src/main/java/com/minsub/java8/time/LocalTimeTest.java
@@ -0,0 +1,11 @@
+package com.minsub.java8.time;
+
+/**
+ * Created by jiminsub on 2016. 5. 22..
+ */
+public class LocalTimeTest {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
